1c95f373a593a81d02ca2998d94486421480201c
sonota <yosiot8753@gmail.com>
2010-09-16 23:57:30 +0900

    procedure-info

--- script-fu-shell-init.scm
+++ script-fu-shell-init.scm
@@ -110,6 +110,49 @@
 (define (function-help func-name)
   (nth 1 (gimp-procedural-db-proc-info func-name)))
 
+(define (zip-args-values names types descs)
+  (if (> (length names) 0)
+      (unbreakupstr
+       (map
+        (lambda (n)
+          (string-append (nth n names)
+                         " ("
+                         (nth n types)
+                         ")\n"
+                         (nth n descs)))
+        (seq (length names)))
+       "\n--\n")
+      "-"))
+
+(define (procedure-args func-name)
+  (zip-args-values
+   (function-args-name func-name)
+   (function-args-type func-name)
+   (function-args-desc func-name)))
+
+(define (procedure-values func-name)
+  (zip-args-values 
+   (function-values-name func-name)
+   (function-values-type func-name)
+   (function-values-desc func-name)))
+
+(define (procedure-info func-name)
+  (define (strip-quote str)
+    (substring str 1 (- (string-length str) 1)))
+
+  (unbreakupstr
+   (map
+    (lambda (x)
+      (string-append "-------- " (car x) " --------\n"
+                     (strip-quote (item->string (cdr x)))))
+    `(("Name" . ,func-name)
+      ("Blurb" . ,(function-blurb func-name))
+      ("Arguments" . ,(procedure-args func-name))
+      ("Return values" . ,(procedure-values func-name))
+      ("Help" . ,(function-help func-name))))
+   "\n\n"))
+
+
 (define (sfs:list-all-functions)
    (cadr (gimp-procedural-db-query "" "" "" "" "" "" "")))
 (define (sfs:list-all-functions:lines)
